071eb95d7e6f1ba0c5e821043d356f32eb6d246a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2020-01-22 13:39:52 -0800

    Initial check in MVC with unit tests

---
+++ Interview2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Interview2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
